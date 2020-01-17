af795082d686a44873c82fbbf0cff8c6d036cf28
Jacob <AvidWriter117@gmail.com>
2020-01-17 14:14:10 -0700

    First steps
    Added planet, capsule, mouselook

---
+++ Assets/Controls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Control tweaking
+    [SerializeField] float mouseSensitivity = 1f;
+
+    //test test test 2 2 2
+
+    private Vector2 mouseMovementSum;
+    private Transform body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.transform.parent.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseMovement();
+        WASDMovement();
+    }
+
+    void MouseMovement()
+    {
+        //How much as the mouse moved in X and Y
+        Vector2 mouseXY = new Vector2 
+            (Input.GetAxisRaw("Mouse X"),
+                Input.GetAxisRaw("Mouse Y"));
+
+        //add new mouse movement to the variable holding camera movement
+        mouseMovementSum += mouseXY;
+
+        //up-down camera movement
+        this.transform.localRotation = Quaternion.AngleAxis(-mouseMovementSum.y, Vector3.right);
+
+        //rotate parent body object left-right, along with camera
+        body.localRotation = Quaternion.AngleAxis(mouseMovementSum.x, Vector3.up);
+    }
+
+    void WASDMovement()
+    {
+        //todo wasd movement of body
+    }
+
+}
